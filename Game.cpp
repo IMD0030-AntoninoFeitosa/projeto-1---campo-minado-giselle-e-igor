1fcbe5f798479cbb4d28edc65299f8df80d185a0
Giselle Neres <38250121+erednasyl@users.noreply.github.com>
2022-04-28 23:52:19 -0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -6,3 +6,21 @@
 
 #include "Game.h"
 
+void show_map(Map map, Difficulty difficulty){
+  
+  Point mapDimensions;
+  
+  if (difficulty == Difficulty::beginner){
+    mapDimensions.x = 10;
+    mapDimensions.y = 10;
+  }
+  else if (difficulty == Difficulty::intermediary){
+    mapDimensions.x = 15;
+    mapDimensions.y = 15;
+  }
+  else if (difficulty == Difficulty::advanced){
+    mapDimensions.x = 15;
+    mapDimensions.y = 30;
+  }
+  
+}
