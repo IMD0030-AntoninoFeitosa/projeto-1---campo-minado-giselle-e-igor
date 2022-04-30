96c2d5505f5fb56efa1ad7b4a5ba196975688118
Giselle Neres <38250121+erednasyl@users.noreply.github.com>
2022-04-30 00:25:41 -0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -6,24 +6,64 @@
 
 #include "Game.h"
 
-void show_map(Map &map, Difficulty difficulty){
+
+Game create_game(Difficulty difficulty){
+  Game gameSetup;
   
   Point mapDimensions;
   
   if (difficulty == Difficulty::beginner){
     mapDimensions.x = 10;
     mapDimensions.y = 10;
+    gameSetup.total_bombs = 10;
   }
   else if (difficulty == Difficulty::intermediary){
     mapDimensions.x = 15;
     mapDimensions.y = 15;
+    gameSetup.total_bombs = 40;
   }
   else if (difficulty == Difficulty::advanced){
     mapDimensions.x = 15;
     mapDimensions.y = 30;
+    gameSetup.total_bombs = 100;
   }
-  
+
+  gameSetup.mapDimensions = mapDimensions;
+
+  return gameSetup;
 }
+
+
+Map create_map(Game game){
+  Map map;
+  Cell genericCell;
+  for (int i = 0; i < game.mapDimensions.x; i++){
+    std::vector<Cell> cellList;
+    for (int j = 0; j < game.mapDimensions.y; j++){
+      cellList.push_back(genericCell);
+    }
+    map.push_back(cellList);
+  }
+
+  return map;
+}
+
+void show_map(Game game, Map map){
+  for (int i = 0; i < game.mapDimensions.x; i++){
+    for (int j = 0; j < game.mapDimensions.y; j++){
+      if (map[i][j].is_hidden == true){
+        std::cout << "º";
+      }
+      else {
+        std::cout << "X";
+      }
+      std::cout << "  ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+
 
 void end_game(bool hasFailed){
   if (hasFailed){
